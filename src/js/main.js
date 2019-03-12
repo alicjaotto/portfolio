ad4665957788bfc254d2c66401a5dfca403a574a
ala <alicja.otto@gmail.com>
2019-03-12 22:27:44 +0100

    working on showing menu only with css

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  function toggleMenu() {
-    const nav = document.querySelector('#nav-menu');
-    nav.classList.toggle('hidden');
-  }
-
-  document.querySelector('.header__nav__toggle')
-    .addEventListener('click', toggleMenu);
+  // function toggleMenu() {
+  //   const nav = document.querySelector('#nav-menu');
+  //   nav.classList.toggle('hidden');
+  // }
+  //
+  // document.querySelector('.header__nav__toggle')
+  //   .addEventListener('click', toggleMenu);
 });
