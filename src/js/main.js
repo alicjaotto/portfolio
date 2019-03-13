556f76fc4d883926f6c4edd14055f7020bb620aa
ala <alicja.otto@gmail.com>
2019-03-14 00:05:14 +0100

    first section for mobile almost ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
   const button = document.querySelector('.header__toggle__button');
+  const links = document.querySelectorAll('.header__nav__menu__list__item');
 
   function toggleMenu() {
     const nav = document.querySelector('#nav-menu');
     nav.classList.toggle('collapse');
     button.classList.toggle('expanded');
-    console.log(button);
   }
 
   button.addEventListener('click', toggleMenu);
+
+  links.forEach((item) => {
+    item.addEventListener('click', toggleMenu);
+  });
 });
