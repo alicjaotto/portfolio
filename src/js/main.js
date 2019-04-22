da8c63d1a28ddfe9d34fc2b6a1d62acb636d3283
ala <alicja.otto@gmail.com>
2019-04-22 22:19:09 +0200

    mobile version almost finished

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const button = document.querySelector('.header__toggle__button');
   const links = document.querySelectorAll('.header__nav__menu__list__item');
   const projects = document.querySelectorAll('.content__projects__project__view__window');
+  const navbar = document.querySelector('.header');
 
   function toggleMenu() {
     const nav = document.querySelector('#nav-menu');
@@ -14,6 +15,14 @@ document.addEventListener('DOMContentLoaded', function(){
   links.forEach((item) => {
     item.addEventListener('click', toggleMenu);
   });
+
+  function addShadow() {
+    var scroll = window.scrollTop;
+    console.log(scroll);
+    navbar.classList.add('shadow');
+  }
+
+  window.addEventListener('scroll', addShadow);
 
   function showLinks() {
     const inner = this.querySelector('.content__projects__project__view__window__inner')
